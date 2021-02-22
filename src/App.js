c1d6f2854d4408b8f710534cad82ab342aad6a0b
James-Kelley-Galvanize <james.kelley@galvanize.com>
2021-02-21 20:35:12 -0700

    Add button via material-ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-
+import react, { useState, useEffect } from "react";
 import "./App.css";
+import Button from "@material-ui/core/Button";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Button variant="contained" color="primary">
+				HELLO WORLD
+			</Button>
+		</div>
+	);
 }
 
 export default App;
